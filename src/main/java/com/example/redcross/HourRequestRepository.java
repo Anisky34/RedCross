35431d7035866ce68e477ebc7f4828a356fa0328
Anisky34 <154300908+Anisky34@users.noreply.github.com>
2025-04-10 07:42:31 +0530

    Update HourRequestRepository.java

--- src/main/java/com/example/redcross/HourRequestRepository.java
+++ src/main/java/com/example/redcross/HourRequestRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
-public interface HourRequestRepository extends JpaRepository<HourRequest, Long> {
+public interface  HourRequestRepository extends JpaRepository<HourRequest, Long> {
     void deleteByVolunteerId(long volunteerId);
     List<HourRequest> findByStatus(String status);
     List<HourRequest> findByVolunteerEmail(String email);
+    List<HourRequest> findByApprovedById(Long approvedById);
+
 }
